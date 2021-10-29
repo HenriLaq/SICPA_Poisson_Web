28a2ba0cd03c6adef10b136f4656d0530f0cc68b
Henri Laquerbe <henri.laquerbe@inrae.fr>
2021-10-29 16:44:06 +0200

    AJOUT pagination mais clonky; depart vacances

--- src/Controller/IndividuController.php
+++ src/Controller/IndividuController.php
@@ -4,22 +4,29 @@ namespace App\Controller;
 
 use App\Entity\LotExploitation;
 use App\Entity\IndividuExploitation;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\IndividuExploitationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
- * @Route("/experimentation")
+ * @Route("/experimentation/lot")
  */
 class IndividuController extends AbstractController
 {
     /**
      * @Route("/{idLot}/individus", name="individu_index")
      */
-    public function index(IndividuExploitationRepository $individuExploitationRepository, LotExploitation $lot): Response
+    public function index(Request $request, IndividuExploitationRepository $individuExploitationRepository, LotExploitation $lot, PaginatorInterface $paginator): Response
     {
         $individus = $individuExploitationRepository->findByLot($lot->getIdLot());
+        $individus = $paginator->paginate(
+            $individus,
+            $request->query->getInt('page', 1),
+            25
+        );
         return $this->render('individu/index.html.twig', [
             'individus' => $individus,
         ]);
