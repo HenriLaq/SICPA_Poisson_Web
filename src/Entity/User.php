6cedd3c8a6ab3ac505ef22031fae4f78f23bbb66
Henri Laquerbe <henri.laquerbe@inrae.fr>
2021-11-16 08:57:42 +0100

    REGENERATION bdd

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,50 +2,82 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
+ * User
+ *
+ * @ORM\Table(name="user", uniqueConstraints={@ORM\UniqueConstraint(name="UNIQ_8D93D649F85E0677", columns={"username"})})
+ * @ORM\Entity
  */
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User
 {
     /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=180, unique=true)
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=180, nullable=false)
      */
     private $username;
 
     /**
-     * @ORM\Column(type="json")
+     * @var array
+     *
+     * @ORM\Column(name="roles", type="json", nullable=false)
      */
-    private $roles = [];
+    private $roles;
 
     /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=false)
      */
     private $password;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="nom_unite", type="string", length=30, nullable=true)
+     */
+    private $nomUnite;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="fin_est_membre", type="date", nullable=true)
+     */
+    private $finEstMembre;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="nom_equipe", type="string", length=30, nullable=true)
+     */
+    private $nomEquipe;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="info_equipe", type="string", length=254, nullable=true)
+     */
+    private $infoEquipe;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @deprecated since Symfony 5.3, use getUserIdentifier instead
-     */
-    public function getUsername(): string
+    public function getUsername(): ?string
     {
-        return (string) $this->username;
+        return $this->username;
     }
 
     public function setUsername(string $username): self
@@ -55,26 +87,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
+    public function getRoles(): ?array
     {
-        return (string) $this->username;
-    }
-
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
+        return $this->roles;
     }
 
     public function setRoles(array $roles): self
@@ -84,10 +99,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
@@ -99,23 +111,56 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * Returning a salt is only needed, if you are not using a modern
-     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
-     *
-     * @see UserInterface
-     */
-    public function getSalt(): ?string
+    public function getNomUnite(): ?string
     {
-        return null;
+        return $this->nomUnite;
     }
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
+    public function setNomUnite(?string $nomUnite): self
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->nomUnite = $nomUnite;
+
+        return $this;
+    }
+
+    public function getFinEstMembre(): ?\DateTimeInterface
+    {
+        return $this->finEstMembre;
+    }
+
+    public function setFinEstMembre(?\DateTimeInterface $finEstMembre): self
+    {
+        $this->finEstMembre = $finEstMembre;
+
+        return $this;
+    }
+
+    public function getNomEquipe(): ?string
+    {
+        return $this->nomEquipe;
+    }
+
+    public function setNomEquipe(?string $nomEquipe): self
+    {
+        $this->nomEquipe = $nomEquipe;
+
+        return $this;
+    }
+
+    public function getInfoEquipe(): ?string
+    {
+        return $this->infoEquipe;
+    }
+
+    public function setInfoEquipe(?string $infoEquipe): self
+    {
+        $this->infoEquipe = $infoEquipe;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getUsername();
     }
 }
