f6842652dcf9f067c4346be95e22c16e5820cf86
Henri Laquerbe <henri.laquerbe@inrae.fr>
2021-12-14 08:58:31 +0100

    SAVE avant tenter localise lot

--- src/Controller/MouvementController.php
+++ src/Controller/MouvementController.php
@@ -6,10 +6,10 @@ use App\Entity\LotExploitation;
 use App\Entity\IndividuExploitation;
 use App\Entity\ExperimentationExploitation;
 use Knp\Component\Pager\PaginatorInterface;
-use App\Repository\LotExploitationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\IndividuExploitationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MouvementController extends AbstractController
@@ -18,9 +18,9 @@ class MouvementController extends AbstractController
     /**
      * @Route("/experimentation/{idExpe}/lot/{idLot}/individu/{idIndividu}/mouvement", name="mouvement_indi_index")
      */
-    public function index_indi(LotExploitationRepository $lotExploitationRepository, IndividuExploitation $indi, Request $request, PaginatorInterface $paginator, LotExploitation $lot, ExperimentationExploitation $expe): Response
+    public function index_indi(IndividuExploitationRepository $individuExploitationRepository, IndividuExploitation $indi, LotExploitation $lot, ExperimentationExploitation $expe): Response
     {
-        $mouvements = $lotExploitationRepository->findMouvByIndi($indi->getIdIndi());
+        $mouvements = $individuExploitationRepository->findMouvByIndi($indi->getIdIndi());
         return $this->render('mouvement/index.html.twig', [
             'idExpe' => $expe->getIdExpe(),
             'idLot' => $lot->getIdLot(),
