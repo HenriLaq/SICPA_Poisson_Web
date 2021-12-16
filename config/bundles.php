d20445f5945fb5a92aff7ebf4cb89a428ed25129
Henri Laquerbe <henri.laquerbe@inrae.fr>
2021-12-16 09:08:53 +0100

    avant moment

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
