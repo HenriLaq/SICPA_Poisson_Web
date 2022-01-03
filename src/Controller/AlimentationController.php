c87649f64311102605a815d342c1c11a6278c091
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-01-03 15:22:41 +0100

    AJOUT compteur de jours de jeune

--- src/Controller/AlimentationController.php
+++ src/Controller/AlimentationController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\JoursDeJeuneType;
 use App\Entity\LotExploitation;
 use App\Entity\ExperimentationExploitation;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AlimentationExploitationRepository;
@@ -14,12 +16,29 @@ class AlimentationController extends AbstractController
     /**
      * @Route("/experimentation/{idExpe}/lot/{idLot}/alimentation", name="alimentation_index")
      */
-    public function index(AlimentationExploitationRepository $alimentationExploitationRepository, ExperimentationExploitation $expe, LotExploitation $lot): Response
+    public function index(Request $request, AlimentationExploitationRepository $alimentationExploitationRepository, ExperimentationExploitation $expe, LotExploitation $lot): Response
     {
         $datas = $alimentationExploitationRepository->findAlimByLot($lot->getIdLot());
+
+        //compter les jours de jeune
+        $form = $this->createForm(JoursDeJeuneType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dates = $form->getData();
+            $dateDiff = $dates['Fin']->diff($dates['Debut'])->format("%a");
+            return $this->render('alimentation/index.html.twig', [
+                'datas' => $datas,
+                'idExpe' => $expe->getIdExpe(),
+                'form' => $form->createView(),
+                'dates' => $dates,
+                'dateDiff' => $dateDiff,
+            ]);
+        }
+
         return $this->render('alimentation/index.html.twig', [
             'datas' => $datas,
-            'idExpe' => $expe->getIdExpe()
+            'idExpe' => $expe->getIdExpe(),
+            'form' => $form->createView(),
         ]);
     }
 }
