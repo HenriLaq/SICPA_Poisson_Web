51444efb7689bba3abaf732d807b4cd9204dc54a
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-01-13 09:16:32 +0100

    avant modif laminas

--- src/Security/LdapFormAuthenticator.php
+++ src/Security/LdapFormAuthenticator.php
@@ -24,7 +24,7 @@ class LdapFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    private UrlGeneratorInterface $urlGenerator;
+    private $urlGenerator;
 
     public function __construct(UrlGeneratorInterface $urlGenerator)
     {
