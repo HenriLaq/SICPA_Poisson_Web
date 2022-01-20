d5109a350922b10f66be5d15fd43deac521fe949
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-01-20 09:23:24 +0100

    AJOUT compteur semaine jeune alimentation

--- src/Controller/AlimentationController.php
+++ src/Controller/AlimentationController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Form\JoursDeJeuneType;
 use App\Entity\LotExploitation;
 use App\Entity\ExperimentationExploitation;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AlimentationExploitationRepository;
@@ -16,12 +15,37 @@ class AlimentationController extends AbstractController
     /**
      * @Route("/experimentation/{idExpe}/lot/{idLot}/alimentation", name="alimentation_index")
      */
-    public function index(Request $request, AlimentationExploitationRepository $alimentationExploitationRepository, ExperimentationExploitation $expe, LotExploitation $lot): Response
+    public function index(AlimentationExploitationRepository $alimentationExploitationRepository, ExperimentationExploitation $expe, LotExploitation $lot): Response
     {
-        $datas = $alimentationExploitationRepository->findAlimByLot($lot->getIdLot());
-
+        $alimentations = $alimentationExploitationRepository->findAlimByLot($lot->getIdLot());
+        $semaines = [];
+        $jeunes = [];
+        $enCours = 0;
+        $i = 0;
+        $j = 0;
+        foreach ($alimentations as $alimentation) {
+            $semaines[$i] = $alimentation->getDateConditionAlim()->format("W");
+            //Si on commence
+            if ($i == 0) {
+                $enCours = $semaines[$i];
+                $jeunes += [$semaines[$i] => 0];
+            }
+            //Si on change de semaine
+            if ($enCours != $semaines[$i]) {
+                $j++;
+                $enCours = $semaines[$i];
+                $jeunes += [$semaines[$i] => 0];
+                //Si on  ne change pas de semaine
+            } else if ($alimentation->getCoeffAlim() == 0) {
+                $jeunes[$semaines[$i]]++;
+            }
+            $i = $i + 1;
+        }
+        //dd($jeunes);
         return $this->render('alimentation/index.html.twig', [
-            'datas' => $datas,
+            'alimentations' => $alimentations,
+            'semaines' => $semaines,
+            'jeunes' => $jeunes,
             'idExpe' => $expe->getIdExpe()
         ]);
     }
