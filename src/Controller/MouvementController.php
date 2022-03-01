50349356054aedf8fdc31b761019711c2ca5a408
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-03-01 10:15:02 +0100

    APRES changement lot mouvement

--- src/Controller/MouvementController.php
+++ src/Controller/MouvementController.php
@@ -3,12 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\LotExploitation;
-use App\Entity\IndividuExploitation;
 use App\Entity\ExperimentationExploitation;
-use App\Repository\LotExploitationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\IndividuExploitationRepository;
+use App\Repository\MouvementExploitationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MouvementController extends AbstractController
@@ -17,13 +15,13 @@ class MouvementController extends AbstractController
     /**
      * @Route("/experimentation/{idExpe}/lot/{idLot}/mouvements", name="mouvement_index")
      */
-    public function index(LotExploitationRepository $lotExploitationRepository, LotExploitation $lot, ExperimentationExploitation $expe): Response
+    public function index(MouvementExploitationRepository $mouvementExploitationRepository, LotExploitation $lot, ExperimentationExploitation $expe): Response
     {
-        $mouvements = $lotExploitationRepository->findHistoByLot($lot->getIdLot());
+        $mouvements = $mouvementExploitationRepository->findHistoByLot($lot->getIdLot());
 
         return $this->render('mouvement/index.html.twig', [
             'idExpe' => $expe->getIdExpe(),
-            'idLot' => $lot->getIdLot(),
+            'Lot' => $lot,
             'mouvements' => $mouvements,
         ]);
     }
