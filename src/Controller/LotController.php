ecdbed02639c63fb541d54cd53d3d521186c28f6
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-03-02 16:28:30 +0100

    AJOUT releve dans mouvement

--- src/Controller/LotController.php
+++ src/Controller/LotController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\IndividuExploitationRepository;
+use App\Repository\ReleveAnimalExploitationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LotController extends AbstractController
@@ -16,13 +17,35 @@ class LotController extends AbstractController
     /**
      * @Route("/experimentation/{idExpe}/lot", name="lot_index")
      */
-    public function index(LotExploitationRepository $lotExploitationRepository, ExperimentationExploitation $expe): Response
+    public function index(LotExploitationRepository $lotExploitationRepository, IndividuExploitationRepository $indiRepo, ReleveAnimalExploitationRepository $relAniRepo, ExperimentationExploitation $expe): Response
     {
         $lots = $lotExploitationRepository->findAllByExpe($expe->getIdExpe());
 
+        /* Courbe des releves 
+        $indis = [];
+        $releveParIndi = [];
+        $releves = [];
+        $lotR = [];
+        foreach($lots as $lot) {
+            array_push($indis, $indiRepo->findAllByLot($lot->getIdLot()));
+            foreach ($indis as $indi){
+                $c = 0;
+                foreach ($indi as $i){
+                    array_push($releveParIndi, $relAniRepo->findRelByIndi($i->getIdIndi()));
+                    $c++;
+                }
+            }
+        }
+        foreach($releveParIndi as $releve){
+            foreach ($releve as $r){
+                array_push($releves, $r);
+            }
+        }
+        //dd($releves);
+        */
         return $this->render('lot/index.html.twig', [
             'lots' => $lots,
-            'idExpe' => $expe->getIdExpe()
+            'idExpe' => $expe->getIdExpe(),
         ]);
     }
 }
--- src/Controller/MouvementController.php
+++ src/Controller/MouvementController.php
@@ -7,6 +7,7 @@ use App\Entity\ExperimentationExploitation;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\MouvementExploitationRepository;
+use App\Repository\ReleveAnimalExploitationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MouvementController extends AbstractController
@@ -15,14 +16,20 @@ class MouvementController extends AbstractController
     /**
      * @Route("/experimentation/{idExpe}/lot/{idLot}/mouvements", name="mouvement_index")
      */
-    public function index(MouvementExploitationRepository $mouvementExploitationRepository, LotExploitation $lot, ExperimentationExploitation $expe): Response
+    public function index(MouvementExploitationRepository $mouvementExploitationRepository, LotExploitation $lot, ExperimentationExploitation $expe, ReleveAnimalExploitationRepository $relAniRepo): Response
     {
         $mouvements = $mouvementExploitationRepository->findHistoByLot($lot->getIdLot());
-
+        $releves = [];
+        foreach ($mouvements as $mouvement){
+            if (sizeof($relAniRepo->findRelByMouv($mouvement->getIdMouvement())) != 0){
+                array_push($releves, $relAniRepo->findOneRelByMouv($mouvement->getIdMouvement()));
+            }
+        }
         return $this->render('mouvement/index.html.twig', [
             'idExpe' => $expe->getIdExpe(),
             'Lot' => $lot,
             'mouvements' => $mouvements,
+            'releves' => $releves
         ]);
     }
 }
