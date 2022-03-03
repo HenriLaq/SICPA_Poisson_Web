30f0d65c3a8a09abd0ef2eb3d0314b84f2e05e45
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-03-03 15:42:12 +0100

    AJOUT graphe lot sans les moyennes.

--- src/Controller/LotController.php
+++ src/Controller/LotController.php
@@ -8,8 +8,10 @@ use App\Repository\LotExploitationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\BassinExploitationRepository;
 use App\Repository\IndividuExploitationRepository;
 use App\Repository\ReleveAnimalExploitationRepository;
+use App\Repository\AlimentationEauExploitationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LotController extends AbstractController
@@ -17,35 +19,48 @@ class LotController extends AbstractController
     /**
      * @Route("/experimentation/{idExpe}/lot", name="lot_index")
      */
-    public function index(LotExploitationRepository $lotExploitationRepository, IndividuExploitationRepository $indiRepo, ReleveAnimalExploitationRepository $relAniRepo, ExperimentationExploitation $expe): Response
+    public function index(LotExploitationRepository $lotExploitationRepository, IndividuExploitationRepository $indiRepo, 
+    ReleveAnimalExploitationRepository $relAniRepo, BassinExploitationRepository $bassinRepo,
+    AlimentationEauExploitationRepository $alimRepo, ExperimentationExploitation $expe): Response
     {
         $lots = $lotExploitationRepository->findAllByExpe($expe->getIdExpe());
 
-        /* Courbe des releves 
-        $indis = [];
+        /* Courbe des releves */
+        $indiParLot = [];
+        $individus = [];
         $releveParIndi = [];
-        $releves = [];
-        $lotR = [];
+
+        $bassins = [];
+        $releveParBassin = [];
+
+        //Pour tous les lots
         foreach($lots as $lot) {
-            array_push($indis, $indiRepo->findAllByLot($lot->getIdLot()));
-            foreach ($indis as $indi){
-                $c = 0;
-                foreach ($indi as $i){
-                    array_push($releveParIndi, $relAniRepo->findRelByIndi($i->getIdIndi()));
-                    $c++;
-                }
+            array_push($indiParLot, $indiRepo->findAllByLot($lot->getIdLot()));
+            array_push($bassins, ($bassinRepo->findBassinById($lot->getIdBassin()))[0]);
+        }
+        
+
+        //Pour tous les grp d'indis par lots
+        foreach ($indiParLot as $indi){
+            //Pour tous les indis
+            foreach ($indi as $i){
+                array_push($releveParIndi, $relAniRepo->findRelByIndi($i->getIdIndi()));
+                array_push($individus, $i);
             }
         }
-        foreach($releveParIndi as $releve){
-            foreach ($releve as $r){
-                array_push($releves, $r);
-            }
+
+        //Pour tous les bassins
+        foreach($bassins as $bassin){
+            array_push($releveParBassin, $alimRepo->findSourceByAlim($bassin->getIdAlimEau()));
         }
-        //dd($releves);
-        */
+        
         return $this->render('lot/index.html.twig', [
             'lots' => $lots,
             'idExpe' => $expe->getIdExpe(),
+            'releveParIndi' => $releveParIndi,
+            'individus' => $individus,
+            'releveParBassin' => $releveParBassin,
+            'bassins' => $bassins,
         ]);
     }
 }
