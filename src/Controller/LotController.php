298d1a43872c6ad0bc01735e8399192a44f9a5ac
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-05-13 11:14:58 +0200

    ADD bilan: relevés entre bons

--- src/Controller/LotController.php
+++ src/Controller/LotController.php
@@ -14,6 +14,8 @@ use App\Repository\IndividuExploitationRepository;
 use App\Repository\MouvementExploitationRepository;
 use App\Repository\ReleveAnimalExploitationRepository;
 use App\Repository\AlimentationEauExploitationRepository;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LotController extends AbstractController
@@ -42,9 +44,57 @@ class LotController extends AbstractController
         }else{
             return $this->render('security/interdit.html.twig', []);
         }
-
-        
     }
+
+    /**
+     * @Route("/experimentation/{idExpe}/lot/{idLot}/form", name="lot_form")
+     */
+    public function form(Request $request, ExperimentationExploitation $expe, LotExploitationRepository $lotExploitationRepository, LotExploitation  $lotExploitation, IndividuExploitationRepository $indiRepo, ReleveAnimalExploitationRepository $relRepo): Response
+    {
+        $lotsExploitation = $lotExploitationRepository->find($lotExploitation->getIdLotExploitation());
+        $indis = $indiRepo->findByLotForm($lotsExploitation->getIdLot());
+        $rels = [];
+        $relsSepares = [];
+        $idParDate = [];
+        foreach($indis as $indi){
+            $rels = $relRepo->findByIndiForm($indi->getIdIndi());
+            foreach($rels as $rel){
+                array_push($relsSepares, $rel);
+                $idParDate[($rel->getDateRelAni())->format('d-m-Y')] = $rel->getIdRelAni();
+            }
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('premier', ChoiceType::class, ['label' => 'Premier relevé', 'choices' => ['Date' => $idParDate],])
+            ->add('dernier', ChoiceType::class, ['label' => 'Dernier relevé', 'choices' => ['Date' => $idParDate]])
+            ->add('valider', SubmitType::class, ['label' => 'Valider'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $relsBons = [];
+            foreach($relsSepares as $rel){
+                if ($rel->getDateRelAni() >= $relRepo->findByRelId($data['premier'])[0]->getDateRelAni() AND $rel->getDateRelAni() <= $relRepo->findByRelId($data['dernier'])[0]->getDateRelAni()){
+                    array_push($relsBons, $rel);
+                }
+            }
+            dd($relsBons);
+            //return $this->redirectToRoute('lot_index', array('idExpe' => $expe->getIdExpe()));
+        }
+
+        return $this->render('lot/bilanZootechnique.html.twig', [
+            'idExpe' => $expe->getIdExpe(),
+            'lots' => $lotsExploitation,
+            'form' => $form->createView(),
+        ]);
+
+    }
+
+
+
+
 
     private function getCourbesBDD($lotsExploitation, $courbeRepo){
         $courbesByLot = [];
