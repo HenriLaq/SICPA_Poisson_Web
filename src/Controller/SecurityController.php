f4a2aa158b6773d65effb4f0493398e63685b48a
Henri Laquerbe <henri.laquerbe@inrae.fr>
2022-06-28 13:16:49 +0200

    SAVE avant essai OIDC

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,23 +20,6 @@ class SecurityController extends AbstractController
      */
     public function login(EntityManagerInterface $entityManager, Request $request, AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        /* AVANT LE COPIE COLLE
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-        */
-
-        // $user = new User();
-        // $entityManager->persist($user);
-        // $entityManager->flush();
-
         $username = $request->request->get('username');
         $password = $request->request->get('password');
         //Récupération des credentials
